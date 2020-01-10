f2926b0e61f63ab7b72f957ce6ec65388a1e202f
BarathA <barath.arumugam@gilead.com>
2020-01-10 13:04:51 +0530

    Initial Commit

---
+++ PracticeMobileApp/PracticeMobileApp/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace PracticeMobileApp.ViewModel
+{
+    class MainPageViewModel : INotifyPropertyChanged
+    {
+
+
+        private string _firstname;
+        private string _lastname;
+        private string _fullname;
+
+
+        public MainPageViewModel()
+        {
+
+            _firstname = "Default First Name";
+            _lastname = "Default Last Name";
+            _fullname = "Default Full Name";
+
+
+        }
+
+        public string FirstName
+        {
+            get { return _firstname; }
+            set { _firstname = value; }
+        }
+
+
+        public string LastName
+        {
+            get { return _lastname; }
+            set { _lastname = value; }
+        }
+
+
+
+        public string FullName
+        {
+            get { return _fullname; }
+            set { _fullname = value; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+    }
+}
